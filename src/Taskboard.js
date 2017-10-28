77347508528a61256f0a46e085cbb56cadbbc32c
Raul Figueira <raulfmiranda@gmail.com>
2017-10-28 11:29:35 -0300

    Usando map para criar estorias

--- src/Taskboard.js
+++ src/Taskboard.js
@@ -3,18 +3,42 @@ import Estoria from './Estoria';
 
 class Taskboard extends Component {
     render() {
+        const estorias = this._getEstorias();
+        const titulo = this._getTitulo(estorias.length); 
+
         return (
             <div>
                 <div className="section no-pad-bot" id="index-banner">
                     <div className="container">
                         <h1 className="header center orange-text">Estórias</h1>
-                        <h3>2 estórias</h3>
-                        <Estoria titulo="Primeiro Titulo" descricao="Primeira descrição" pontos="10"/>
-                        <Estoria titulo="Segundo Titulo" descricao="Segunda descrição" pontos="20"/>
+                        <h3>{titulo}</h3>
+                        {estorias}
                     </div>
                 </div>
             </div>
         );
+    }
+
+    _getEstorias() {
+        const estorias = [
+            {id: 1, titulo: 'Contratar Seguro', descricao: 'Como usuário...', pontos: 10},
+            {id: 2, titulo: 'Cancelar Seguro', descricao: 'Como usuário...', pontos: 30},
+        ];
+
+        return estorias.map( estoria => 
+            <Estoria
+                titulo={estoria.titulo} descricao={estoria.descricao}
+                pontos={estoria.pontos} key={estoria.id} />);
+    }
+
+    _getTitulo(totalDeEstorias) {
+        if (totalDeEstorias === 0) {
+            return "Backlog vazio";
+        } else if (totalDeEstorias === 1) {
+            return "1 estória";
+        } else {
+            return `${totalDeEstorias} estórias`;
+        }
     }
 }
 
